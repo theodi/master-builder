8fc045be1659721d96a19d608de33dfa4d145c35
James Smith <james@floppy.org.uk>
2013-06-28 15:01:05 +0100

    Add ability to ignore certain branches

--- master-builder.groovy
+++ master-builder.groovy
@@ -10,6 +10,10 @@ def projects = [
   'services-manager'
 ]
 
+def ignoreBranches = [
+  'gh-pages'
+]
+
 projects.each {
   def projectName = it
 
@@ -20,116 +24,120 @@ projects.each {
   // Generate a job for each branch
   branches.each { 
     def branchName = it.name
+    
+    // Check ignored branches
+    if (!ignoreBranches.contains(branchName)) {
+    
+      job {
 
-    job {
+        // Job name
+        name "${projectName}-${branchName}".replaceAll('/','-')
 
-      // Job name
-      name "${projectName}-${branchName}".replaceAll('/','-')
-
-      // Git configuration
-      scm {
-        git("git@github.com:theodi/${projectName}.git", branchName)
-      }
-  
-      // Trigger builds on github pushes
-      configure { project ->
-        project/triggers << "com.cloudbees.jenkins.GitHubPushTrigger" {
-          spec ""
+        // Git configuration
+        scm {
+          git("git@github.com:theodi/${projectName}.git", branchName)
         }
-      }
   
-      // Configure build wrappers
-      configure { project ->
-        // use colour xterm output
-        project/buildWrappers << "hudson.plugins.ansicolor.AnsiColorBuildWrapper" {
-          colorMapName "xterm"
-        }
-        // Use RVM environment '.'
-        project/buildWrappers << "ruby-proxy-object" {
-          ruby-object("ruby-class":"Jenkins::Plugin::Proxies::BuildWrapper", pluginid:"rvm") {
-            object("ruby-class":"RvmWrapper", pluginid:"rvm") {
-              impl(pluginid:"rvm", "ruby-class":"String", ".")
-            }
-            pluginid(pluginid:"rvm", "ruby-class":"String", "rvm")
-          }
-        }            
-      }
-  
-      // Build steps: shell script
-      steps {
-          shell("""\
-#!/bin/bash
-source /var/lib/jenkins/env
-[[ -s 'package.json' ]] && npm install
-[[ -s 'Gemfile' ]] && bundle --without=production
-[[ -s 'db' ]] && rake db:migrate
-rake""")
-      }
-
-      // Publishers
-      configure { project ->
-        
-        // CI game
-        project/publishers << "hudson.plugins.cigame.GamePublisher" {}
-        
-        // Mail notifications
-        project/publishers << "hudson.tasks.Mailer" {
-          recipients "tech@theodi.org"
-          dontNotifyEveryUnstableBuild "false"
-          sendToIndividuals "true"
-        }
-        
-        // Coverage
-        project/publishers << "hudson.plugins.rubyMetrics.rcov.RcovPublisher" {
-          reportDir "coverage/rcov"
-          targets {
-            "hudson.plugins.rubyMetrics.rcov.model.MetricTarget" {
-              metric "TOTAL_COVERAGE"
-              healthy "90"
-              unhealthy "75"
-              unstable "0"
-            }
-            "hudson.plugins.rubyMetrics.rcov.model.MetricTarget" {
-              metric "CODE_COVERAGE"
-              healthy "90"
-              unhealthy "75"
-              unstable "0"
-            }
+        // Trigger builds on github pushes
+        configure { project ->
+          project/triggers << "com.cloudbees.jenkins.GitHubPushTrigger" {
+            spec ""
           }
         }
+  
+        // Configure build wrappers
+        configure { project ->
+          // use colour xterm output
+          project/buildWrappers << "hudson.plugins.ansicolor.AnsiColorBuildWrapper" {
+            colorMapName "xterm"
+          }
+          // Use RVM environment '.'
+          project/buildWrappers << "ruby-proxy-object" {
+            ruby-object("ruby-class":"Jenkins::Plugin::Proxies::BuildWrapper", pluginid:"rvm") {
+              object("ruby-class":"RvmWrapper", pluginid:"rvm") {
+                impl(pluginid:"rvm", "ruby-class":"String", ".")
+              }
+              pluginid(pluginid:"rvm", "ruby-class":"String", "rvm")
+            }
+          }            
+        }
+  
+        // Build steps: shell script
+        steps {
+            shell("""\
+  #!/bin/bash
+  source /var/lib/jenkins/env
+  [[ -s 'package.json' ]] && npm install
+  [[ -s 'Gemfile' ]] && bundle --without=production
+  [[ -s 'db' ]] && rake db:migrate
+  rake""")
+        }
+
+        // Publishers
+        configure { project ->
         
-        // Some post-build tasks for master
-        if(branchName == "master") {
-          // Post-build release tagging if on master
-          project/publishers << "hudson.plugins.postbuildtask.PostbuildTask" {
-            tasks {
-              "hudson.plugins.postbuildtask.TaskProperties" {
-                logTexts {}
-                "EscalateStatus" "true"
-                "RunIfJobSuccessful" "true"
-                script """\
-cd \$WORKSPACE;
-git tag release-\$BUILD_ID;
-git push origin release-\$BUILD_ID;
-git tag -f CURRENT;
-git push origin CURRENT"""
+          // CI game
+          project/publishers << "hudson.plugins.cigame.GamePublisher" {}
+        
+          // Mail notifications
+          project/publishers << "hudson.tasks.Mailer" {
+            recipients "tech@theodi.org"
+            dontNotifyEveryUnstableBuild "false"
+            sendToIndividuals "true"
+          }
+        
+          // Coverage
+          project/publishers << "hudson.plugins.rubyMetrics.rcov.RcovPublisher" {
+            reportDir "coverage/rcov"
+            targets {
+              "hudson.plugins.rubyMetrics.rcov.model.MetricTarget" {
+                metric "TOTAL_COVERAGE"
+                healthy "90"
+                unhealthy "75"
+                unstable "0"
+              }
+              "hudson.plugins.rubyMetrics.rcov.model.MetricTarget" {
+                metric "CODE_COVERAGE"
+                healthy "90"
+                unhealthy "75"
+                unstable "0"
               }
             }
           }
-          // Push features to relish if available
-          project/publishers << "hudson.plugins.postbuildtask.PostbuildTask" {
-            tasks {
-              "hudson.plugins.postbuildtask.TaskProperties" {
-                logTexts {}
-                "EscalateStatus" "false"
-                "RunIfJobSuccessful" "true"
-                script """\
-#!/bin/bash
-source "/var/lib/jenkins/.rvm/scripts/rvm" && rvm use .
-[[ -s 'features' ]] && bundle exec relish push theodi/${projectName}"""
+        
+          // Some post-build tasks for master
+          if(branchName == "master") {
+            // Post-build release tagging if on master
+            project/publishers << "hudson.plugins.postbuildtask.PostbuildTask" {
+              tasks {
+                "hudson.plugins.postbuildtask.TaskProperties" {
+                  logTexts {}
+                  "EscalateStatus" "true"
+                  "RunIfJobSuccessful" "true"
+                  script """\
+  cd \$WORKSPACE;
+  git tag release-\$BUILD_ID;
+  git push origin release-\$BUILD_ID;
+  git tag -f CURRENT;
+  git push origin CURRENT"""
+                }
               }
             }
-          }          
+            // Push features to relish if available
+            project/publishers << "hudson.plugins.postbuildtask.PostbuildTask" {
+              tasks {
+                "hudson.plugins.postbuildtask.TaskProperties" {
+                  logTexts {}
+                  "EscalateStatus" "false"
+                  "RunIfJobSuccessful" "true"
+                  script """\
+  #!/bin/bash
+  source "/var/lib/jenkins/.rvm/scripts/rvm" && rvm use .
+  [[ -s 'features' ]] && bundle exec relish push theodi/${projectName}"""
+                }
+              }
+            }          
+          }
         }
       }
     }
