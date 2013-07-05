1f1f9273f731b7042791b678793677ef282aac6a
James Smith <james@floppy.org.uk>
2013-07-05 10:38:38 +0100

    Enable github build notifications

--- master-builder.groovy
+++ master-builder.groovy
@@ -163,6 +163,9 @@ source "/var/lib/jenkins/.rvm/scripts/rvm" && rvm use .
             }
           }
           
+          // Github notification
+          project/publishers << "com.cloudbees.jenkins.GitHubCommitNotifier"(plugin: "github@1.6") {}
+
           // Mail notifications
           project/publishers << "hudson.tasks.Mailer" {
             recipients "tech@theodi.org"
